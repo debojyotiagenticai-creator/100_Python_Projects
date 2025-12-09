8086542d0539522a4f96955f6cb705abfa5fe978
debojyotiagenticai-creator <debojyotiagenticai@gmail.com>
2025-12-09 13:16:56 +0530

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,9 @@
+# This is a Band Name Generator Program
+# This program takes two inputs, City Name and Pet name and concatenates it to produce a Band Name
+
+print("Welcome To Band Name Generator Program")
+
+city = input("Please enter the name of the city you live in :\n")
+pet = input("Please enter the name of your pet :\n")
+
+print("The name of your Band could be : "+ city + " " + pet)
